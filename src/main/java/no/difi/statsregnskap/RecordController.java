31dd1ef60efecfe28742de3aa65da9fd7cd076af
Claus Guttesen <kometen@gmail.com>
2018-05-24 10:24:49 +0200

    Changed hashmap-definition
    Changed to string, string to make it a proper object. This way I can add new elements to the hashmap.

--- src/main/java/no/difi/statsregnskap/RecordController.java
+++ src/main/java/no/difi/statsregnskap/RecordController.java
@@ -22,13 +22,13 @@ public class RecordController {
 	@Autowired
 	private RecordRepository recordRepository;
 	
-	@RequestMapping(value = "/record/{year}", method = RequestMethod.GET)
-	public List<HashMap<String, Record>> recordByYear(@PathVariable("year") String year) {
+	@RequestMapping(value = "/record", method = RequestMethod.GET)
+	public List<HashMap<String, String>> records() {
         BigDecimal ytd = new BigDecimal("0.0"); // Year To Date
         // <Unique identifier, Period (month), Pair<amount (numeric(19,3))>>, accumulate YTD
         Table<String, Integer, Pair> tAmount = TreeBasedTable.create();
 
-	    List<HashMap<String, Record>> l = recordRepository.findByYear(year);
+	    List<HashMap<String, String>> l = recordRepository.getAllRecords();
 
 	    // Add records to guava sorted table.
 	    for (int i = 0; i < l.size(); i++) {
@@ -41,21 +41,78 @@ public class RecordController {
 	        }
 	    }
 	    
-        // Traverse and accumulate YTD.
+        // Traverse guava table and accumulate YTD.
         for (String item : tAmount.rowKeySet()) {
             System.out.println("guava rowKeySet(): " + item);
             ytd = BigDecimal.ZERO;
             for (Entry<Integer, Pair> period : tAmount.row(item).entrySet()) {
                 ytd = ytd.add(period.getValue().getAmount());
                 period.getValue().setYTDamount(ytd);
-                System.out.println("period: " + period.getKey() + ", amount: " + period.getValue().getAmount() + ", YTD: " + period.getValue().getYTDamount());
+                //System.out.println("period: " + period.getKey() + ", amount: " + period.getValue().getAmount() + ", YTD: " + period.getValue().getYTDamount());
             }
         }
 
+        // Add YTD from guava table.
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).get("uniqueIdentifier") != null) {
+                String unique = String.valueOf(l.get(i).get("uniqueIdentifier")); // Uniquely identifies record
+                Integer period = Integer.parseInt(String.valueOf(l.get(i).get("period"))); // Period, as month like 9
+                Pair p = tAmount.get(unique, period);
+                l.get(i).put("ytdamount", p.getYTDamount().toString());
+                //System.out.println("ytd: " + p.getYTDamount());
+            }
+        }
+        
         System.out.println(l.getClass());
 	    System.out.println(l.get(1967).get("orgnr"));
 
 	    return l;
 	}
 
+    @RequestMapping(value = "/record/{year}", method = RequestMethod.GET)
+    public List<HashMap<String, String>> recordByYear(@PathVariable("year") String year) {
+        BigDecimal ytd = new BigDecimal("0.0"); // Year To Date
+        // <Unique identifier, Period (month), Pair<amount (numeric(19,3))>>, accumulate YTD
+        Table<String, Integer, Pair> tAmount = TreeBasedTable.create();
+
+        List<HashMap<String, String>> l = recordRepository.findByYear(year);
+
+        // Add records to guava sorted table.
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).get("uniqueIdentifier") != null) {
+                String unique = String.valueOf(l.get(i).get("uniqueIdentifier")); // Uniquely identifies record
+                Integer period = Integer.parseInt(String.valueOf(l.get(i).get("period"))); // Period, as month like 9
+                String periodamount = String.valueOf(l.get(i).get("periodamount")).replace(',', '.');
+                //System.out.println(String.valueOf(l.get(i).get("uniqueIdentifier")) + ", "+ String.valueOf(l.get(i).get("periodamount")));
+                tAmount.put(unique, period, new Pair(new BigDecimal(periodamount)));
+            }
+        }
+        
+        // Traverse guava table and accumulate YTD.
+        for (String item : tAmount.rowKeySet()) {
+            System.out.println("guava rowKeySet(): " + item);
+            ytd = BigDecimal.ZERO;
+            for (Entry<Integer, Pair> period : tAmount.row(item).entrySet()) {
+                ytd = ytd.add(period.getValue().getAmount());
+                period.getValue().setYTDamount(ytd);
+                //System.out.println("period: " + period.getKey() + ", amount: " + period.getValue().getAmount() + ", YTD: " + period.getValue().getYTDamount());
+            }
+        }
+
+        // Add YTD from guava table.
+        for (int i = 0; i < l.size(); i++) {
+            if (l.get(i).get("uniqueIdentifier") != null) {
+                String unique = String.valueOf(l.get(i).get("uniqueIdentifier")); // Uniquely identifies record
+                Integer period = Integer.parseInt(String.valueOf(l.get(i).get("period"))); // Period, as month like 9
+                Pair p = tAmount.get(unique, period);
+                l.get(i).put("ytdamount", p.getYTDamount().toString());
+                //System.out.println("ytd: " + p.getYTDamount());
+            }
+        }
+        
+        System.out.println(l.getClass());
+        System.out.println(l.get(1967).get("orgnr"));
+
+        return l;
+    }
 }
